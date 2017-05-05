ea846cfb213e49bfb0bacfea1cb03756e6475553
Lyle Underwood <lyleunderwood@gmail.com>
2017-05-05 04:05:46 -0600

    don't need this anymore

--- download-chromium.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-CHROMIUM_ROOT="http://commondatastorage.googleapis.com/chromium-browser-snapshots/Linux_x64"
-CHROMIUM_LATEST=`wget -q -O - "$CHROMIUM_ROOT/LAST_CHANGE"`
-wget $CHROMIUM_ROOT/$CHROMIUM_LATEST/chrome-linux.zip
