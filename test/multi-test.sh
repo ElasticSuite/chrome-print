786ab0987061bc38156d5fd2675d0a63c79bd6db
Lyle Underwood <lyleunderwood@gmail.com>
2017-07-11 15:05:57 -0600

    add multi-test.sh

---
+++ test/multi-test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+./test.sh results/1 &
+./test.sh results/2 &
+./test.sh results/3 &
+./test.sh results/4 &
+./test.sh results/5 &
+./test.sh results/6 &
--- test/test.sh
+++ test/test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+out="${1-test}.pdf"
 port=`docker ps |grep chromeprint_print |sed 's/.*:\([0-9]*\)-.*/\1/'`
-curl -F "htmlFile=@test.html" -F "width=12.3" -F "height=7.6" -X POST -H "Content-Type: multipart/form-data" -o test.pdf http://localhost:$port/
+curl -F "htmlFile=@test.html" -F "width=12.3" -F "height=7.6" -X POST -H "Content-Type: multipart/form-data" -o $out http://localhost:$port/
